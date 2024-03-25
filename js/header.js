006c202f35083434c143b3e71e8c84d81037d95a
FilipStraka <straka168@gmail.com>
2024-03-25 15:40:32 +0100

    pridanie eventlistenera click na hambgurer icon

--- js/header.js
+++ js/header.js
@@ -3,4 +3,6 @@ const menuIcon = document.querySelector(".menu-icon");
 const navigation = document.querySelector("nav");
 const hamburgerIcon = document.querySelector(".fa-solid");
 
-console.log(menuIcon,navigation,hamburgerIcon);
+menuIcon.addEventListener("click", () => {
+    
+});
