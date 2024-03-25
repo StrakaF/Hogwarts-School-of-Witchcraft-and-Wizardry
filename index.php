42e4c3e14ec23d9e866951091610cccecfa2d049
FilipStraka <straka168@gmail.com>
2024-03-25 23:07:19 +0100

    pridané query pre index.php a nonbreaking space v h1

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Úvodná strana</title>
-    <!-- Links -->
+    <!-- Css links -->
     <link rel="stylesheet" href="./css/general.css">
     <link rel="stylesheet" href="./css/header.css">
-    <link rel="stylesheet" href="./query/header-query.css">
     <link rel="stylesheet" href="./css/index.css">
+    <!-- Query links -->
+    <link rel="stylesheet" href="./query/header-query.css">
+    <link rel="stylesheet" href="./query/index-query.css">
+    <!-- Fontawesome icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
@@ -19,7 +22,7 @@
     <main>
         <section class="main-heading">
                 <img src="./img/hogwarts-logo.png" alt="Logo Bradavice">
-                <h1>Škola čarov a kúziel</h1>
+                <h1>Škola čarov a&nbsp;kúziel</h1>
                 <h2>Bradavice</h2>
         </section>
 
