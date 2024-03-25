4bdee97dc71f6c4dae43c98caff370d5a22a02f5
FilipStraka <straka168@gmail.com>
2024-03-25 16:04:57 +0100

    po kliknuti na hamburger icon sa nacigacia vysunie a po kliknuti na X icon sa schova

--- js/header.js
+++ js/header.js
@@ -7,8 +7,12 @@ menuIcon.addEventListener("click", () => {           //po kliknuti
     if (hamburgerIcon.classList[1] === "fa-bars") { //ked je icon hamburger
         hamburgerIcon.classList.remove("fa-bars"); //removni ju
         hamburgerIcon.classList.add("fa-xmark"); //a pridaj X icon
+
+        navigation.style.display = "block"; //vyskoci navigacia
     } else {
         hamburgerIcon.classList.remove("fa-xmark"); //ked je icon X, removni ju
         hamburgerIcon.classList.add("fa-bars");    //a nastav naspat hamburger icon
+
+        navigation.style.display = "none"; //zmizne navigacia
     }
 });
