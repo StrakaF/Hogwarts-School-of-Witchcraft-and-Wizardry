0abd0f99df7787d555d5cc8191bc4cf5e8aa1f82
FilipStraka <straka168@gmail.com>
2024-03-26 13:35:42 +0100

    úprava dalších chýb v headeri, kód by mal byť teraz v poriadku

--- ziaci.php
+++ ziaci.php
@@ -20,7 +20,6 @@ $students = getAllStudents($connection,"id, first_name, second_name");
     <link rel="stylesheet" href="./css/footer.css">
     <!-- Query links -->
     <link rel="stylesheet" href="./query/header-query.css">
-    <link rel="stylesheet" href="./query/index-query.css">
     <!-- Fontawesome icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
