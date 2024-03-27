73345300158c4eb61d9096edf7892ed40d996ad2
FilipStraka <straka168@gmail.com>
2024-03-27 22:46:07 +0100

    Vytvorenie headeru pre administráciu

---
+++ assets/admin-header.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="logo">
+        <a href="../index.php">
+            <img src="../img/hogwarts-logo.png" alt="Hogwarts-logo">
+        </a>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="ziaci.php">Zoznam žiakov</a></li>
+            <li><a href="pridat-ziaka.php">Pridať žiaka</a></li>
+        </ul>
+    </nav>
+
+    <div class="menu-icon">
+        <i class="fa-solid fa-bars"></i>
+    </div>
+</header>
