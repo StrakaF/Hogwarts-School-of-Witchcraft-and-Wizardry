9db064e968fbb939a20a1a015c322d0f519910c8
FilipStraka <straka168@gmail.com>
2024-03-28 22:42:58 +0100

    Zahashovanie hesla zregistračného  formuláru uloženého v premennej

--- admin/after-registration.php
+++ admin/after-registration.php
@@ -7,10 +7,12 @@ session_start();
 
 if($_SERVER["REQUEST_METHOD"] === "POST") {
 
-    $connection = connecionDB();
+    $connection = connectionDB();
 
     $first_name = $_POST["first-name"];
     $second_name = $_POST["second-name"];
     $email = $_POST["email"];
-    $password = $_POST["password"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+
 }
