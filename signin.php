84eb12b59ae995c55b173a7c007e048ccd711df8
FilipStraka <straka168@gmail.com>
2024-03-30 18:14:08 +0100

    Na rovnakej úrovni ako index.php sme vytvorili súbor signin.php a pridali sme tam základné links na CSS, ikonky, JS a require header + footer

---
+++ signin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Úvodná strana</title>
+    <!-- Css links -->
+    <link rel="stylesheet" href="./css/general.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <!-- Query links -->
+    <link rel="stylesheet" href="./query/header-query.css">
+    <!-- Fontawesome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    
+    <?php require "assets/header.php"; ?>
+
+        
+
+    <?php require "assets/footer.php"; ?>
+
+    <script src="./js/header.js"></script>
+</body>
+</html>
