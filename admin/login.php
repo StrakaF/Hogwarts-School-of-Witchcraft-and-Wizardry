4fc16973c9a4f7ce7d47e44b9e28cfe378de1e51
FilipStraka <straka168@gmail.com>
2024-03-30 18:40:10 +0100

    Vytiahnutie dát z signin formulára

--- admin/login.php
+++ admin/login.php
@@ -6,7 +6,15 @@ require "../assets/user.php";
 
 session_start();
 
+// Kontrola, či bola požiadavka odoslaná metódou POST
 if($_SERVER["REQUEST_METHOD"] === "POST" ) {
+    // Kód ktorý sa vykoná len v prípade, že bola odoslaná požiadavka POST
 
+    // Pripojenie do DB
     $conn = connectionDB();
+    // Vytiahnutie dát z signin formulára
+    $log_email = $_POST["login-email"];
+    $log_password = $_POST["login-password"];
+
+
 }
