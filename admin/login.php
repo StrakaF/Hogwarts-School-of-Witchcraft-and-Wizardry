103452efed74da0df102a358820bc2acd7393965
FilipStraka <straka168@gmail.com>
2024-03-31 23:21:20 +0200

    V súbore login.php sme pridali HTML s podmienkou že Ak error nieje prázdny, tak ho vypíš, to znamená že uživateľ zadal nesprávne heslo a v php kóde sa vykonal else statement

--- admin/login.php
+++ admin/login.php
@@ -32,8 +32,25 @@ if($_SERVER["REQUEST_METHOD"] === "POST" ) {
         redirectUrl("/Bradavice-projekt/admin/ziaci.php");
 
     } else {
-        // Neúspešné prihlásenie
+        $error = "Chyba pri prihlasení";
     }
 
    
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Ak error nieje prázdny, tak ho vypíš -->
+    <?php if(!empty($error)): ?>
+        <p><?= $error ?></p>
+    <?php endif; ?>
+</body>
+</html>
