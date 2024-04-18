936aab51d7190e98abb68697e9ece75962087a84
FilipStraka <straka168@gmail.com>
2024-04-18 23:19:32 +0200

    Pridanie CSS štýlov a fontawesome linku do headeru v photos.php

--- admin/photos.php
+++ admin/photos.php
@@ -18,6 +18,14 @@ $user_id = $_SESSION["logged_in_user_id"]; // Uloźenie ID usera zo session po r
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Css links -->
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <!-- Query links -->
+    <link rel="stylesheet" href="../query/header-query.css">
+    <!-- Fontawesome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
