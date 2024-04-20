c902899298f7a616221dd737af307a50f59c3504
FilipStraka <straka168@gmail.com>
2024-04-20 21:48:48 +0200

    V upload-photos.php sme requirely Database.php, Auth.php a Url.php, spustili session a pridali overenie či je užívateľ prihlásený

---
+++ admin/upload-photos.php
@@ -0,0 +1,17 @@
+<?php
+
+require "../classes/Database.php";
+require "../classes/Auth.php";
+require "../classes/Url.php";
+
+session_start();
+
+// Overenie či je užívateľ prihlásený 
+if( !Auth::isLoggedIn() ){  // Ak skončí false (nieje prihlásený), prepneme na true
+    die("Nepovolený prístup"); // a vykoná sa die, inak sa pokračuje v programe
+}
+
+
+
+
+?>
