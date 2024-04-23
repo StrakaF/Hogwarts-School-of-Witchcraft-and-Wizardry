8727c75f848a49e4876781e48aa1e96c8241a440
FilipStraka <straka168@gmail.com>
2024-04-23 22:39:23 +0200

    Vo vytvorenej funkcií insertImage v classes Image sme vytvorili SQL query a prichystali sme dotaz na databázu, ktorý sa neskôr vykoná a je uložený v premennej

--- classes/Image.php
+++ classes/Image.php
@@ -2,7 +2,10 @@
 
 class Image {
 
-    public static function insertImage(){
+    public static function insertImage($conn, $user_id, $image_name){
+        $sql = "INSERT INTO image (user_id, image_name)
+                VALUES (:user_id, :image_name)";
         
+        $stmt = $conn->prepare($sql);
     }
 }
