07c0eb8f1aca8cf0aa3cdc1a87084c2bd74fd839
FilipStraka <straka168@gmail.com>
2024-04-23 22:47:00 +0200

    Nabindovali sme :user_id a :image_name reálnymi hodnotami

--- classes/Image.php
+++ classes/Image.php
@@ -7,5 +7,12 @@ class Image {
                 VALUES (:user_id, :image_name)";
         
         $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(":user_id", $user_id, PDO::PARAM_INT);
+        $stmt->bindValue(":image_name", $image_name, PDO::PARAM_STR);
+
+        if($stmt->execute()) {
+            return true;
+        }
     }
 }
