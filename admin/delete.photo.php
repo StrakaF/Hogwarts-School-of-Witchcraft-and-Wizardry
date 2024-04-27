0051247d60bc503e52b5c612481b7bd81a74cf94
FilipStraka <straka168@gmail.com>
2024-04-27 22:26:40 +0200

    Začatie session a pridanie overenia ći je user prihlásený, stále v súbore delete-photo.php

--- admin/delete.photo.php
+++ admin/delete.photo.php
@@ -5,8 +5,12 @@ require "../classes/Auth.php";
 require "../classes/Image.php";
 require "../classes/Url.php";
 
+session_start();
 
-
+// Overenie či je user prihlásený
+if( !Auth::isLoggedIn() ){  // Ak skončí false (nieje prihlásený), prepneme na true
+    die("Nepovolený prístup"); // a vykoná sa die, inak sa pokračuje v programe
+}
 
 
 
