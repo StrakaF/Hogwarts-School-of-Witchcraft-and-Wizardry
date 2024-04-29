212dcb07895859b87c48bf9bba2dba28b5b48558
FilipStraka <straka168@gmail.com>
2024-04-29 22:11:27 +0200

    V súbore error-page.php sme vymazali header a footer + css a ikonky, a pridali len odkaz na students.php

--- errors/error-page.php
+++ errors/error-page.php
@@ -11,24 +11,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Css links -->
     <link rel="stylesheet" href="../css/general.css">
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/footer.css">
-    <!-- Query links -->
-    <link rel="stylesheet" href="../query/header-query.css">
-    <!-- Fontawesome icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Error page</title>
 </head>
 <body>
-    <?php require "../assets/admin-header.php" ?>
 
     <main>
         <section class="error">
             <p><?= $error_text ?></p>
         </section>
+
+        <section class="link">
+            <a href="../admin/students.php">Späť na úvodnú stranu administrácie</a>
+        </section>
     </main>
 
-    <?php require "../assets/footer.php" ?>
     <script src="../js/header.js"></script>
 </body>
 </html>
