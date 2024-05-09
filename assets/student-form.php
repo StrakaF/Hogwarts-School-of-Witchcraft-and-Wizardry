80b47f9a78632608c74bf87c88c7c5637ea313d6
FilipStraka <straka168@gmail.com>
2024-05-09 23:06:55 +0200

    Naštýlovaný main element + prehodenie poradia inputov vo formulári (life sme dali na spod, college nad to)

--- assets/student-form.php
+++ assets/student-form.php
@@ -6,30 +6,32 @@
             placeholder="Krstné meno"
             value ="<?= htmlspecialchars($first_name) ?>"
             required>
-    <br>
+    
     <input type="text"
             name ="second_name"
             placeholder="Priezvisko"
             value ="<?= htmlspecialchars($second_name) ?>"
             required>
-    <br>
+    
     <input type="number"
             name ="age"
             min="10"
             placeholder="Aktuálny vek"
             value ="<?= htmlspecialchars($age) ?>"
             required>
-    <br>
-    <textarea name="life"
-                placeholder="Podrobnosti o žiakovi"
-                required><?= htmlspecialchars($life) ?></textarea>
-    <br>
+    
     <input type="text"
             name ="college"
             placeholder="Názov internátu"
             value ="<?= htmlspecialchars($college) ?>"
-            required>
-        <br>
+            required>    
+
+    <textarea name="life"
+                placeholder="Podrobnosti o žiakovi"
+                required><?= htmlspecialchars($life) ?></textarea>
+    
+    
+        
     <input type="submit" value="Uložiť">
 </form>
 
