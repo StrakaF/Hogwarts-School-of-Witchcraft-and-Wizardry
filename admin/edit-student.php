07f2c58eb40a533bba85b1f7abf5ec7cb3987f0c
FilipStraka <straka168@gmail.com>
2024-05-09 23:24:42 +0200

    Vytvorený súbor admin-edit-student-query.css vo folderi query

--- admin/edit-student.php
+++ admin/edit-student.php
@@ -66,6 +66,7 @@
     <link rel="stylesheet" href="../css/admin-edit-student.css">
     <!-- Query links -->
     <link rel="stylesheet" href="../query/header-query.css">
+    <link rel="stylesheet" href="../query/admin-edit-student-query.css">
     <!-- Fontawesome icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Editovanie žiaka</title>
