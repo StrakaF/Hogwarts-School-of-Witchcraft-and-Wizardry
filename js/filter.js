acac1eb2c90e81461005f0f3cc8d251fe71f00ca
FilipStraka <straka168@gmail.com>
2024-05-10 23:14:33 +0200

    Na input sme pridali eventListenera ktorý reaguje udalosť s názvom input

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,26 @@
-// Constants 
+// CONSTANTS
+
+// Input for filtering and searching
 const input = document.querySelector(".filter-input");
+
+// One student (NodeList)
 const allOneStudents = document.querySelectorAll(".one-student");
+
+// One student converted to array
+const allOneStudentsArray = Array.from(allOneStudents);
+
+// Div with all students on page
 const allStudentsDiv = document.querySelector(".all-students");
 
+// Students to object
+const studentsObjects = allOneStudentsArray.map( (oneStudent, index) => {
+    return {
+        id: index,
+        studentsName: oneStudent.querySelector("h2").textContent,
+        studentsLink: oneStudent.querySelector("a")
+    }
+});
+
+input.addEventListener("input", () => {
+    
+})
